f11e9fa2a9f1017bbecc3056ad54ddf87107dca8
Fahim Ahmed <99824146+ahmedfahim21@users.noreply.github.com>
2023-02-12 21:31:59 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 //Middleware
 app.use(cors({
-    origin: ["http://localhost:3000", "http://task-app-mern-fahim.onrender.com/"],}
+    origin: ["http://localhost:3000", "https://task-app-mern-fahim.onrender.com/"],}
 ));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
