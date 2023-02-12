2d9c5e869b372cd9b712a2f20199c85c25b2b980
Fahim Ahmed <ahmed.fahim0207@gmail.com>
2023-02-12 19:45:04 +0530

    removed frontend

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,9 @@ const cors = require("cors");
 const app = express();
 
 //Middleware
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:3000", "http://task-app-mern-fahim.onrender.com/"],}
+));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use("/api/tasks", taskRoute);
